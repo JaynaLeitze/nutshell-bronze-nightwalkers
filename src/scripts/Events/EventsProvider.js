d6cc84888e677a0a4426f8ed583d54806d8cd12d
Jayna Leitze <jayna.leitze@gmail.com>
2020-11-17 10:12:29 -0600

    fetch events from database.json

---
+++ src/scripts/Events/EventsProvider.js
@@ -0,0 +1,16 @@
+let events = [];
+
+export const useEvents = () => {
+  return events.slice();
+};
+
+export const getEvents = () => {
+  return (
+    fetch(`http://localhost:8088/events`)
+      .then((response) => response.json())
+      //do something with the data
+      .then((parsedEvents) => {
+        events = parsedEvents;
+      })
+  );
+};
