d1dc1312a8824ba1740ce15bdda64c4b09d1be8f
Jayna Leitze <jayna.leitze@gmail.com>
2020-11-18 15:59:58 -0600

    working on weather functionality

---
+++ src/scripts/Weather/WeatherMaker.js
@@ -0,0 +1,36 @@
+import { getWeather, useWeather } from "./WeatherProvider.js";
+
+export const weatherMaker = () => {
+  getWeather().then(() => {
+    const eventWeather = useWeather();
+    const htmlTarget = document.querySelector(".weatherDisplay");
+    const fiveDayWeather = eventWeather.daily.slice(0, 5);
+    console.log(fiveDayWeather);
+    const weatherHTML = fiveDayWeather
+      .map((day) => {
+        const datept1 = day.dt * 1000;
+        const humanDate = new Date(datept1);
+        const condensedDate = humanDate.toLocaleDateString("en-US", {
+          month: "numeric",
+          day: "numeric",
+          year: "numeric",
+        });
+        const high = day.temp.max;
+        const low = day.temp.min;
+        const precip = day.weather[0].description;
+        const iconAddress = `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+        return `
+                <div class="weatherDay">
+                    <img src="${iconAddress}">
+                    <p class="date">${condensedDate}</p>
+                    <p class="highTemp">High: ${high}</p>
+                    <p class="lowTemp">Low: ${low}</p>
+                    <p class="rainCondition">Forecast: ${precip}</p>
+                </div>
+            `;
+      })
+      .join("");
+
+    htmlTarget.innerHTML = weatherHTML;
+  });
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-
 import { LoginForm } from "./auth/LoginForm.js";
 import { RegisterForm } from "./auth/RegisterForm.js";
 import { Nutshell } from "./Nutshell.js";
@@ -6,15 +5,11 @@ import { taskButton } from "./tasks/TaskButton.js";
 import { taskForm } from "./tasks/TaskForm.js";
 import { getTasks } from "./tasks/TaskProvider.js";
 import { TaskList } from "./tasks/TaskList.js";
-import {useArticles,getArticles} from "./articles/articleprovider.js"
-import {articleList} from "./articles/articleList.js"
-import {articleForm} from"./articles/articleform.js"
-import {articleButton} from"./articles/articleButton.js"
+import { useArticles, getArticles } from "./articles/articleprovider.js";
+import { articleList } from "./articles/articleList.js";
+import { articleForm } from "./articles/articleform.js";
+import { articleButton } from "./articles/articleButton.js";
 import { newEventButton } from "./Events/EventButton.js";
-import { taskButton } from "./tasks/TaskButton.js";
-import { taskForm } from "./tasks/TaskForm.js";
-import { getTasks } from "./tasks/TaskProvider.js";
-import { TaskList } from "./tasks/TaskList.js";
 import { EventForm } from "./Events/EventsForm.js";
 import { EventList } from "./Events/EventsList.js";
 import { getEvents } from "./Events/EventsProvider.js";
@@ -27,9 +22,7 @@ import { getEvents } from "./Events/EventsProvider.js";
         ensure that the Nutshell component gets rendered
 */
 
- 
-getArticles()
-.then(useArticles)
+getArticles().then(useArticles);
 LoginForm();
 RegisterForm();
 Nutshell();
@@ -39,7 +32,7 @@ taskForm();
 TaskList();
 articleList();
 articleForm();
-articleButton()
+articleButton();
 newEventButton();
 EventForm();
 getEvents().then(EventList);
