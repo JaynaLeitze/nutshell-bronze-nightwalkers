31927035813074a6a46fbf4e36c999083e074b4c
Suganya <suganprakash0621@gmail.com>
2020-11-18 15:45:41 -0600

    modified main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,10 +11,6 @@ import {articleList} from "./articles/articleList.js"
 import {articleForm} from"./articles/articleform.js"
 import {articleButton} from"./articles/articleButton.js"
 import { newEventButton } from "./Events/EventButton.js";
-import { taskButton } from "./tasks/TaskButton.js";
-import { taskForm } from "./tasks/TaskForm.js";
-import { getTasks } from "./tasks/TaskProvider.js";
-import { TaskList } from "./tasks/TaskList.js";
 import { EventForm } from "./Events/EventsForm.js";
 import { EventList } from "./Events/EventsList.js";
 import { getEvents } from "./Events/EventsProvider.js";
