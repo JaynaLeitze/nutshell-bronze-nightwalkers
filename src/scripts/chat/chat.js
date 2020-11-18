dfb3759d97eefbf6a02e6f12cc688938e0bf9ba5
Susan Wiens <susanmichellewiens@gmail.com>
2020-11-18 10:12:55 -0600

    chat.js

--- src/scripts/chat/chat.js
+++ src/scripts/chat/chat.js
@@ -2,7 +2,7 @@
 // html format of what the message should look like
 // should accept one parameter which is messageObject
 
-export const generateMessageHtml = (messageObject) => {
+export const generateMessageHtmlcreate  = (messageObject) => {
     return `
         <div class="single-message">
             ${messageObject.chatText}
